a4bb3d0f4aa0bd423fb515cd749d7b9258bf4a11
ethanethan838 <147289338+ethanethan838@users.noreply.github.com>
2025-07-07 15:16:01 +0700

    Harus kasi nama

---
+++ HomePage.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from operator import imod
+
+for i in range(7):
+    st.markdown("")
+
+st.html("""
+
+    <style>
+        .titleheader{
+            font-size : 100px;
+        }
+        
+        .
+
+    </style>
+            
+""")
+
+st.html("<h1 class='titleheader' style='bottom-margin : 0;'>StaySharp</h1>")
+st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rutrum, nibh et vulputate rutrum, est turpis sollicitudin felis, eu pharetra arcu ante vitae justo.")
