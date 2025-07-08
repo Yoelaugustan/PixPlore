321d1e04e7752ac0ee736705ba66b7ae3e643e7e
Yoel Augustan <yoelagustan@gmail.com>
2025-07-08 17:40:31 +0700

    Fix HomePage

--- HomePage.py
+++ HomePage.py
@@ -1,109 +1,94 @@
 import streamlit as st
 
-st.markdown("""
+st.set_page_config(layout="wide")
+
+st.html("""
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100..900&display=swap" rel="stylesheet">
 
 <style>
-    .texttitle{
-            font-size: 135px;
-            font-weight: bold;
-            margin: 0;
-            padding: 0;
-            color: #000000;
-            text-shadow: 0px 1.75px 4px black;
-    }
-        .textslogan{
-            font-size: 35px;
-            font-weight: bold;
-    }
-        .textsubslogan{
-            font-size: 30px;
-    }
-            
-    @media only screen and (max-width: 600px) {
-        .texttitle{
-            font-size: 60px;
-            font-weight: bold;
-            margin: 0;
-            padding: 0;
-            color: #000000;
-            text-shadow: 0px 1.75px 4px black;
-        }
-        .textslogan{
-            font-size: 15px;
-             font-weight: bold;
-        }
-        .textsubslogan{
-            font-size: 13px;
-        }
-    }
-            
-            
-    html, body, .page {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-    }
+section.main > div {
+    max-width: 100vw;
+}
+.stApp > header, .stApp > footer {
+    display: none;
+}
+.stApp .main .block-container {
+    padding: 0;
+    margin: 0;
+}
 
-    .barlow-semibold {
-        font-family: "Barlow", sans-serif;
-        font-weight: 600;
-        font-style: normal;
-        text-shadow: 0px 1px 2px black;
-    }        
-    
-    .mainbg {
-        width: 100%;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        //background-image: url('GradientMesh.png')
-        //background-image: linear-gradient(to right, #2da1d6 , #66dbff);
-        background: radial-gradient(at 30% 20%, #f06acb, transparent 60%),
-            radial-gradient(at 70% 25%, #6f5cff, transparent 60%),
-            radial-gradient(at 30% 80%, #c6ffc6, transparent 60%),
-            radial-gradient(at 90% 90%, #6a84b5, transparent 60%);
-        background-color: #d4d9ff;
-        padding: 0 10vw;
-        box-sizing: border-box;
-    }
+.stApp {
+    background: radial-gradient(at 30% 20%, #f06acb, transparent 60%),
+        radial-gradient(at 70% 25%, #6f5cff, transparent 60%),
+        radial-gradient(at 30% 80%, #c6ffc6, transparent 60%),
+        radial-gradient(at 90% 90%, #6a84b5, transparent 60%);
+    background-color: #d4d9ff;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 100vh;
+}
 
-    .titleback {
-        //background-image: linear-gradient(to right, #2da1d6 , #66dbff);
-        border-radius: 132px;
-        //padding: 35px 130px 35px 88px;
-        width: 90%;
-    }
+section[data-testid="stMain"] {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}       
 
-    .titleheader {
+[data-testid="stSidebar"] {
+    background: linear-gradient(#000b14, #001627) !important;
+    color: white !important;
+}
+[data-testid="stSidebar"] * {
+    color: white !important;
+}
 
+.texttitle {
+    font-size: 6vw;
+    font-weight: 800;
+    color: #000000;
+    margin: 0;
+    padding: 0;
+    text-shadow: 0px 1.75px 4px black;
+}
+.textslogan {
+    font-size: 2vw;
+    font-weight: 600;
+    color: #000000;
+    margin: 0;
+    padding: 0;
+    text-shadow: 0px 1px 2px black;
+}
+.textsubslogan {
+    font-size: 1.7vw;
+    font-weight: 400;
+    color: #000000;
+    margin: 0;
+    padding: 0;
+}
+
+@media only screen and (max-width: 600px) {
+    .texttitle {
+        font-size: 12vw;
     }
-    section.main > div {
-        max-width: 98rem;
+    .textslogan {
+        font-size: 5vw;
     }
-    .stApp .main .block-container {
-        padding: 0rem;
-        padding-top: 1.72rem;
+    .textsubslogan {
+        font-size: 4vw;
     }
-    [data-testid=stSidebar] {
-        background-image: linear-gradient(#000b14, #001627);
-        color: white;
-    }
+}
 </style>
 
-<body class='page'>
-    <div class='mainbg' background='GradientMesh.png'>
-        <div class='titleback'>
-            <p class='texttitle'>PixPlore</p>
-            <p class = 'barlow-semibold, textslogan', style='margin-top: 0px; margin-bottom: 5px; padding: 0; color: #000000; font-style:barlow;'>Learning through lenses, growing through curiosity.</p>
-            <p class = 'barlow-semibold, textsubslogan', style='margin-top: 10px; padding: 0;color: #000000;'>
-                Where ordinary objects become valuable lessons, simply through a lens.
-            </p>
-        </div>
+<div style="display: flex; align-items: center; height: 100%;">
+    <div class='titleback'>
+        <p class='texttitle'>PixPlore</p>
+        <p class='textslogan'>Learning through lenses, growing through curiosity.</p>
+        <p class='textsubslogan'>
+            Where ordinary objects become valuable lessons, simply through a lens.
+        </p>
     </div>
-</body>
-""", unsafe_allow_html=True)
+</div>
+""")
